c2affcdeb3de52beced8862678145d8300106423
roan1088 <rohan_188@yahoo.com>
2020-10-20 23:54:55 -0400

    Updated the markdown to include all the information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+  ${data.description}
 
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.installation}
+  \`\`\`
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  This project is licensed under the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  # Tests
+  To run tests, run the following command:
+  \`\`\`
+  ${data.tests}
+  \`\`\`
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.githubName}](https://github.com/${data.githubName}).
 `;
 }
 
