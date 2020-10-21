fabbdfdf1c08a8e0c66381a9ac8df1fa35f39a59
roan1088 <rohan_188@yahoo.com>
2020-10-20 22:32:05 -0400

    Added question prompts

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // Including the file system module 
 var fs = require("fs");
+var inquirer = require("inquirer");
 
 // array of questions for user
 const questions = [
-
+    {type: "input", message: "What is your Github username?", name: "githubName"},
+    {type: "input", message: "What is your email address?", name: "email"},
+    {type: "input", message: "What is the title of your project?", name: "title"},
+    {type: "input", message: "Provide a short description of your project", name: "description"},
+    {type: "list", message: "What kind of license should your project have?", name: "license",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
+    {type: "input", message: "What command should be run to install dependenies?", name: "installation",
+    default: "npm i"},
+    {type: "input", message: "What command should be run to run tests?", name: "tests",
+    default: "npm test"},
+    {type: "input", message: "What does the user need to know about using the repo?", name: "usage"},
+    {type: "input", message: "What does the user need to know about contributing to the repo?", name: "contributing"}
 ];
 
 // function to write README file
@@ -18,7 +30,9 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then(function(response) {
+        console.log(response);
+    })
 }
 
 // function call to initialize program
