ec1c3821b81cbda37e4086535d485fb87bcacbb1
roan1088 <rohan_188@yahoo.com>
2020-10-21 20:48:25 -0400

    Added input validation function to rqeuire github username, email, title and description

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const inquirer = require("inquirer");
 // Including the generateMarkdown.js module
 const generateMarkdown = require("./utils/generateMarkdown")
 
+// Function for input validation to require an input
+function inputRequired(userInput) {
+    if (userInput) {
+        return true;
+    }
+    return "Input is required";
+}
+
 // array of questions for user
 const questions = [
-    {type: "input", message: "What is your Github username?", name: "githubName"},
-    {type: "input", message: "What is your email address?", name: "email"},
-    {type: "input", message: "What is the title of your project?", name: "title"},
-    {type: "input", message: "Provide a short description of your project", name: "description"},
+    {type: "input", message: "What is your Github username?", name: "githubName", validate: inputRequired},
+    {type: "input", message: "What is your email address?", name: "email", validate: inputRequired},
+    {type: "input", message: "What is the title of your project?", name: "title", validate: inputRequired},
+    {type: "input", message: "Provide a short description of your project", name: "description", validate: inputRequired},
     {type: "list", message: "What kind of license should your project have?", name: "license",
     choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
     {type: "input", message: "What command should be run to install dependenies?", name: "installation",
